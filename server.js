53f327bb0230a2cd02ee54496c55f12e620add7c
esmahcn <esmahacene@example.com>
2025-09-09 09:51:25 +0100

    v4

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ import { connectDB } from './config/db.js';
 import { SECRETS } from './config/secrets.js';
 import userRoutes from "./routes/userRoutes.js";
 import blogRoutes from "./routes/blogRoutes.js";
+import cors from 'cors';
+
 dotenv.config();
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Routes
