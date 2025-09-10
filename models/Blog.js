85ef5c0985c0902c1364703890b0fbf9ac596198
esmahcn <esmahacene@example.com>
2025-09-10 09:51:43 +0100

    v7

--- models/Blog.js
+++ models/Blog.js
@@ -4,11 +4,11 @@ const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Reference to the User model
-    required: true,
-  },
+user: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "User",
+  required: false, // <-- was true, now optional
+},
 });
 
 export default mongoose.model("Blog", blogSchema);
